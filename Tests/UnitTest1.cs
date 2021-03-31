6266e6f469bb5835b10c41eafc3c1943b81e2887
Ptit-Biscuit <vincent.brebion@epsi.fr>
2021-03-31 17:02:41 +0200

    Add tests

--- Tests/UnitTest1.cs
+++ Tests/UnitTest1.cs
@@ -1,6 +1,8 @@
 using LuccaDevises;
 using System;
 using System.IO;
+using LuccaDevises.Exceptions;
+using LuccaDevises.Services;
 using Xunit;
 
 namespace Tests
@@ -22,17 +24,33 @@ namespace Tests
         [Fact]
         public void NoneExistingFile()
         {
-            Assert.Equal(-1, Program.Main(new []{ "DoesNotExists.toto" }));
+            Assert.Equal(-1, Program.Main(new[] { "DoesNotExists.toto" }));
         }
 
         [Theory]
         [InlineData("empty")]
         [InlineData("emptyFirstLine")]
         [InlineData("missingStartCurrency")]
-        [InlineData("emptySecondLine")]
         public void InvalidFileData(string path)
         {
-            Assert.Equal(-1, Program.Main(new[] { Path.GetFullPath(@$"..\\..\\..\\Resources\\${path}.txt") }));
+            Assert.Equal(-1, Program.Main(new[] { Path.GetFullPath(@$"../../../Resources/{path}.txt") }));
+        }
+
+        [Theory]
+        [InlineData("empty")]
+        [InlineData("emptySecondLine")]
+        [InlineData("missingExchangeRate")]
+        public void Graph_shouldNotGenerateAValidGraphFromInvalidFile(string path)
+        {
+            Assert.Throws<GraphException>(() =>
+                GraphService.CreateGraph(Path.GetFullPath(@$"../../../Resources/{path}.txt")));
+        }
+
+        [Fact]
+        public void Graph_shouldOutputExchangeRate()
+        {
+            var graph = GraphService.CreateGraph(Path.GetFullPath("../../../Resources/valid.txt"));
+            Assert.Equal(59033, decimal.Ceiling(550 * GraphService.ExchangeRate(graph, "EUR", "JPY")));
         }
     }
 }
